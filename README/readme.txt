2bbf14bca44e16360f4a14a43a70c709923067e2
Kingsley Oha <KingsleyOha@Names-MacBook-Air.local>
2019-05-29 13:45:21 +0200

    ReadMe Note added in ReadMe folder

---
+++ README/readme.txt
@@ -0,0 +1,14 @@
+
+(a)Pull the master branch from bitbucket and open with VS CODE or your preferred IDE
+(b) ( If you want to use the zip file, unzip the folder in your desktop and open with VS CODE or your preferred IDE )
+
+From the terminal: Navigate to the project folder and Run "npm install to install"
+From the terminal Run "npm start" 
+Open go to the browser and enter: "http://localhost:3000" 
+---Leave the server runing
+Open another terminal and run "npm run electron"
+
+The application should be running on both the web browser and and on the local machine (Both MAC and Windows)
+
+
+Please Note that you might need to install some dependencies that are compatible with your machine/computer
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,15 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyAEPFEuZsSY3gqgx1MNipHP5ccRGgOydhk',
-  authDomain: 'contact-app1-f47a6.firebaseapp.com',
-  databaseURL: 'https://contact-app1-f47a6.firebaseio.com',
-  projectId: 'contact-app1-f47a6',
-  storageBucket: 'contact-app1-f47a6.appspot.com',
-  messagingSenderId: '881057084561',
-  appId: '1:881057084561:web:3319b1f872df8186'
+  
+apiKey: 'AIzaSyAEPFEuZsSY3gqgx1MNipHP5ccRGgOydhk',
+authDomain: 'contact-app1-f47a6.firebaseapp.com',
+databaseURL: 'https://contact-app1-f47a6.firebaseio.com',
+projectId: 'contact-app1-f47a6',
+storageBucket: 'contact-app1-f47a6.appspot.com',
+messagingSenderId: '881057084561',
+appId: '1:881057084561:web:3319b1f872df8186'
+
 };
 
 firebase.initializeApp(config);
@@ -15,3 +17,16 @@ firebase.initializeApp(config);
 const database = firebase.database();
 
 export { firebase, database as default };
+
+
+
+
+
+
+// apiKey: "AIzaSyCrKM792JdoV3nyc5tV-dBVf3V3m0ONw3s",
+//   authDomain: "contact-app-e5e8f.firebaseapp.com",
+//   databaseURL: "https://contact-app-e5e8f.firebaseio.com",
+//   projectId: "contact-app-e5e8f",
+//   storageBucket: "contact-app-e5e8f.appspot.com",
+//   messagingSenderId: "150279404093",
+//   appId: "1:150279404093:web:663d14ce52f53acc"
