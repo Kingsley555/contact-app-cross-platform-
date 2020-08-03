55eef0f59b10709d01c893e963e8c8a40c2636a3
Kingsley Oha <KingsleyOha@Names-MacBook-Air.local>
2020-08-03 13:20:29 +0200

    added api key for testing

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,13 @@
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 const config = {
-  
-apiKey: 'AIzaSyAEPFEuZsSY3gqgx1MNipHP5ccRGgOydhk',
-authDomain: 'contact-app1-f47a6.firebaseapp.com',
-databaseURL: 'https://contact-app1-f47a6.firebaseio.com',
-projectId: 'contact-app1-f47a6',
-storageBucket: 'contact-app1-f47a6.appspot.com',
-messagingSenderId: '881057084561',
-appId: '1:881057084561:web:3319b1f872df8186'
-
+  apiKey: "",
+  authDomain: "contact-app1-f47a6.firebaseapp.com",
+  databaseURL: "https://contact-app1-f47a6.firebaseio.com",
+  projectId: "contact-app1-f47a6",
+  storageBucket: "contact-app1-f47a6.appspot.com",
+  messagingSenderId: "881057084561",
+  appId: "1:881057084561:web:3319b1f872df8186",
 };
 
 firebase.initializeApp(config);
@@ -18,12 +16,9 @@ const database = firebase.database();
 
 export { firebase, database as default };
 
-
-
-
-
-
+//test
 // apiKey: "AIzaSyCrKM792JdoV3nyc5tV-dBVf3V3m0ONw3s",
+//AIzaSyAEPFEuZsSY3gqgx1MNipHP5ccRGgOydhk
 //   authDomain: "contact-app-e5e8f.firebaseapp.com",
 //   databaseURL: "https://contact-app-e5e8f.firebaseio.com",
 //   projectId: "contact-app-e5e8f",
